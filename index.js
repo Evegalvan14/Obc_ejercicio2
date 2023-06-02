4f7bbfb36c94c292e61697ff7bfe240bf6243edf
Evelin Galvan <evelingalvan.eg@hotmail.com>
2023-06-01 22:35:07 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const ejercicio2 = {
+	name        : 'Evelin Galván',
+	age         : 33,
+	developer   : false,
+	date        : new Date(1989, 8, 14),
+	favoriteBook: {
+		title : 'Origen',
+		author: 'Dan Brown',
+		date  : new Date(2017, 9, 3),
+		url   : 'https://danbrown.com/origin/'
+	},
+}
+
+console.log(ejercicio2.favoriteBook.date)
